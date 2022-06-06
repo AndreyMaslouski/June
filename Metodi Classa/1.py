e2f6f728239a03e3af774aaafcd8adc8f85e6942
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-06 12:08:17 +0300

    Создание нового "динамического" Класса, введение переменной в него.

--- Metodi Classa/1.py
+++ Metodi Classa/1.py
@@ -1,16 +1,52 @@
 # Методы Класса
 
-class MyClass():
-    @staticmethod
-    def staticmethod():
-        print('static method called')
+# class MyClass():
+#     @staticmethod
+#     def staticmethod():
+#         print('static method called')
 
-class children_My_class(MyClass):
-    pass
-my_obj = MyClass()
-my_obj.staticmethod()
-my_obj_1=children_My_class()
-my_obj_1.staticmethod()
+# class children_My_class(MyClass):
+#     pass
+# my_obj = MyClass()
+# my_obj.staticmethod()
+# my_obj_1=children_My_class()
+# my_obj_1.staticmethod()
+
+# class Person():
+#     @staticmethod
+#     def is_adult(age):
+#         if age > 18:
+#             return True
+#         else:
+#             return False
+
+# print(Person.is_adult(15))
+
+class MyClass():
+    TOTAL_OBJECTS = 0
+
+    def __init__(self):
+        MyClass.TOTAL_OBJECTS = MyClass.TOTAL_OBJECTS + 1
+
+    @classmethod
+    def total_objects(cls):
+        print("Total objects: ", cls.TOTAL_OBJECTS)
+
+# Создаём объекты
+my_obj1 = MyClass()
+my_obj2 = MyClass()
+my_obj3 = MyClass()
+
+#Вызываем classmethod
+MyClass.total_objects()
+
+
+
+
+
+
+
+
 
 
 # class Calc:
