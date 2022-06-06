735d6a8086df52721309ea2e83660377165b56bd
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-07 02:25:07 +0300

    Внёс изменения

--- D/Met_Class_1.py
+++ D/Met_Class_1.py
@@ -6,19 +6,18 @@ class Person:
     age_1 = 15
     age_2 = 17
     def __init__(self,name):
-        self.name_1 = name
-        self.name_2 = name
+        self.name = name
 
     def __del__(self):
-        print(self.name_1,"удален из памяти")
+        print(self.name,"удален из памяти")
 
 
     def Info(self):
         print(self.age_1)
         print(self.age_2)
-        print("Привет, меня зовут",self.name_1)
+        print("Привет, меня зовут",self.name)
         print("Мне",self.age_1,"лет")
-        print("Привет, меня зовут", self.name_2)
+        print("Привет, меня зовут", self.name)
         print("Мне", self.age_2, "лет")
 
 person_1 = Person("Mikle")
@@ -26,6 +25,26 @@ person_1.Info()
 del person_1
 person_2 = Person("Никита")
 person_2.Info()
+
+# Статический метод
+class River:
+    @staticmethod
+    def get_class_details():
+        print("Это класс River")
+
+class Car(River):
+    def staticmethod(self):
+        print("Это класс Car")
+
+
+my_dream = River
+my_dream.get_class_details()
+my_dream_1 = Car
+my_dream_1.staticmethod("BMW")
+
+River.get_class_details()
+
+
 
 
 
