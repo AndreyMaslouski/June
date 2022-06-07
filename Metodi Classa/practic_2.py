33b3ab4eeee503b61d65045a5ecc78e14d63a6c9
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-08 01:00:59 +0300

    Модифицируем наш класс Rectangle

--- Metodi Classa/practic_2.py
+++ Metodi Classa/practic_2.py
@@ -1,23 +1,39 @@
+# class Rectangle:
+#     def __init__(self,width,height):
+#         self._width=width
+#         self._height=height
+
+#     def get_width(self):
+#         return self._width
+
+#     def set_width(self,w):
+#         self._width = w
+
+#     def get_height(self):
+#         return self._height
+
+#     def set_height(self,h):
+#         self._height = h
+
+#     def area(self):
+#         return self._width*self._height
+
+# rect=Rectangle(10,20)
+# rect.get_width()
+# rect._width
+
+# Модифицируем наш класс Rectangle:
 class Rectangle:
+
     def __init__(self,width,height):
-        self._width=width
-        self._height=height
+        self.__width = width
+        self.__height = height
 
     def get_width(self):
-        return self._width
+        return self.__width
 
     def set_width(self,w):
-        self._width = w
+        self.__width = w
 
-    def get_height(self):
-        return self._height
 
-    def set_height(self,h):
-        self._height = h
 
-    def area(self):
-        return self._width*self._height
-
-rect=Rectangle(10,20)
-rect.get_width()
-rect._width
