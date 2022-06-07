e093cfc06e4ae642325ecf3f3424a1acca6c407e
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-08 02:02:20 +0300

    Вывод разных уровней доступа

--- D/Urovni_dostupa_1.py
+++ D/Urovni_dostupa_1.py
@@ -1,12 +1,29 @@
 # Продемонстрируйте разные уровни доступа на примере любого класса
 
 class Example:
-    def __init__(self,tea,coffee):           # __init__ private level
+    def __init__(self,tea,coffee,area):           # __init__ private level
         self.__tea = tea
         self.__coffee = coffee               # private
-
-    def get_tea(self):                       # protected
+        self.__area = area
+    def get_tea(self):
         return self.__tea                    # private
 
-    def set_tea(self,t):                    # protected
+    def set_tea(self,t):
         self.__tea = t                      # private
+
+    def get_coffee(self):
+        return self.__coffee
+
+    def set_coffee(self,c):
+        self.__coffee = c
+
+    def area(self):
+        return self.__tea*self.__coffee*self.__area
+
+
+Rect = Example(7,11,25)
+print(Rect._Example__tea)              # Вывод private
+print(Rect._Example__coffee)           # Вывод private
+print(Rect._Example__area)             # Вывод private
+print(Rect.get_tea())                  # Вывод public
+print(Rect.area())                     # Вывод public
