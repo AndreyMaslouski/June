4a9be1ab91b9c11b60223cb25e05829de4754c2e
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-08 00:40:20 +0300

    Написание спец. методов, чтоб обратиться к атрибутам

--- Metodi Classa/practic_2.py
+++ Metodi Classa/practic_2.py
@@ -2,3 +2,10 @@ class Rectangle:
     def __init__(self,width,height):
         self._width=width
         self._height=height
+
+    def get_width(self):
+        return self._width
+
+    def set_width(self,w):
+        self._width = w
+
