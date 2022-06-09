2b767b1f12fd103503af415c60f4f87eed26a222
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-09 11:09:25 +0300

    Создал новый Класс. И в нём init и super.init, которые наследуют функции и методы родит. класса, и передают их в init класса-Ребенка.

--- Practica/Human.py
+++ Practica/Human.py
@@ -14,11 +14,13 @@
 class Human:
     default_name = "No name"
     default_age = 0
+
     def __init__(self, name=default_name, age=default_age):
         self.name = name
         self.age = age
         self.__money = 0
         self.__house = None
+
     def info(self):
         print(f'Name: {self.name}')
         print(f'Age: {self.age}')
@@ -28,26 +30,37 @@ class Human:
     @staticmethod
     def default_info():
         print(f'Default name: {Human.default_name}')
-        print (f'Default age: {Human.default_age}')
+        print(f'Default age: {Human.default_age}')
 
-    def earn_money(self,amount):
+    def earn_money(self, amount):
         self.__money += amount
         print(f"Вы заработали {amount}. У Вас {self.__money} денег")
 
-class House:
+
 # 1
-    def __init__(self,area,price):
+class House:
+    # 2,3
+    def __init__(self, area, price):
         self._area = area
         self._price = price
-# 2
-    def final_price(self,discount):
-        final_price = self._price*(100-discount)/100
+
+    # 4
+    def final_price(self, discount):
+        final_price = self._price * (100 - discount) / 100
         return final_price
 
 
-if __name__=='__main__':
+# 2.1
+class SmallHouse(House):
+    default_area = 40
+
+    def __init__(self, price):
+        super.__init__(SmallHouse.default_area, price)
+
+
+if __name__ == '__main__':
     Human.default_info()
-    Alex = Human('Alex',25)
+    Alex = Human('Alex', 25)
     Alex.info()
     Alex.earn_money(10000)
     Alex.earn_money(5000)
