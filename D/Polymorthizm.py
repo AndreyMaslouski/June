08f9c630a332bbda30015acda6e4060d5d64c004
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-09 20:12:13 +0300

    Написание своих примеров полиморфизма.

---
+++ D/Polymorthizm.py
@@ -0,0 +1,50 @@
+# Придумайте свой примеры видов полиморфизма. Прикрепите скриншоты или файл с ними.
+
+class Future:
+    def __init__(self,street):
+        self.__street = street
+
+    @property
+    def street(self):
+        return self.__street
+
+    @street.setter
+    def street(self,d):
+        self.__street = d
+
+    def info(self):
+        print("Future")
+        print("Street: " + self.__street)
+
+class Present(Future):
+
+    def __init__(self,sunny,rain,street):
+        super().__init__(street)
+        self.__sunny = sunny
+        self.__rain = rain
+
+    @property
+    def sunny(self):
+        return self.__sunny
+
+    @sunny.setter
+    def sunny(self,s):
+        if s =="Yes":
+            self.__sunny = s
+        else:
+            raise ValueError
+
+    @property
+    def rain(self):
+        return self.__rain
+
+    @rain.setter
+    def rain(self,r):
+        if r =="Yes":
+            self.__rain = r
+        else:
+            raise ValueError
+
+
+
+
