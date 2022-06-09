21a1088e83f8ce35ce961f8b8150275aaaadb450
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-09 18:51:09 +0300

    Создал класс Rectangle, добавил в него init и дополнительные методы, которые понадобятся.

--- Metodi Classa/polymorth_practic.py
+++ Metodi Classa/polymorth_practic.py
@@ -18,3 +18,33 @@ class Figure:
     def info(self):
         print("Figure")
         print("Color: " + self.__color)
+
+
+class Rectangle(Figure):
+
+    def __init__(self, width, height, color):
+        super().__init__(color)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        if w > 0:
+            self.__width = w
+        else:
+            raise ValueError
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        if h > 0:
+            self.__height = h
+        else:
+            raise ValueError
