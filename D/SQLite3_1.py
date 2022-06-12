2ec1891c3ee59587a650f9412d0832360e33e228
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-12 05:58:28 +0300

    Создал SQLite-Таблицу с нужными мне колонками-столбцами

---
+++ D/SQLite3_1.py
@@ -0,0 +1,41 @@
+# 1.Сформулируйте SQL запрос для создания таблицы book. Структура таблицы book:
+#
+# Поле
+#
+# Тип, описание
+#
+# book_id
+#
+# INT PRIMARY KEY AUTO_INCREMENT
+#
+# title
+#
+# VARCHAR(50)
+#
+# author
+#
+# VARCHAR(30)
+#
+# price
+#
+# DECIMAL(8, 2)
+#
+# amount
+#
+# INT
+#
+# 2.Занесите новую строку в таблицуbook
+#
+# 3.Выбрать информацию о всех книгах из таблицы book.
+
+import sqlite3
+
+#1 Создаём Таблицу book:
+base = sqlite3.connect('book.db')
+cur = base.cursor()
+
+base.execute('CREATE TABLE IF NOT EXISTS book(book_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,title VARCHAR(50),author VARCHAR(30),price DECIMAL(8,2), amount INT)'.format('book'))
+base.commit()
+
+r = cur.execute('SELECT*FROM book').fetchall()
+print(r)
