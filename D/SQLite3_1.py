ce5f1e5082d2d2a77e67f2b17115df4a7f8fc902
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-12 06:38:26 +0300

    Выбрал всю информацию по ней

--- D/SQLite3_1.py
+++ D/SQLite3_1.py
@@ -24,7 +24,7 @@
 #
 # INT
 #
-# 2.Занесите новую строку в таблицуbook
+# 2.Занесите новую строку в таблицу book
 #
 # 3.Выбрать информацию о всех книгах из таблицы book.
 
@@ -34,8 +34,17 @@ import sqlite3
 base = sqlite3.connect('book.db')
 cur = base.cursor()
 
-base.execute('CREATE TABLE IF NOT EXISTS book(book_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,title VARCHAR(50),author VARCHAR(30),price DECIMAL(8,2), amount INT)'.format('book'))
-base.commit()
+# base.execute('CREATE TABLE IF NOT EXISTS book(book_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,title VARCHAR(50),''author VARCHAR(30),price DECIMAL(8,2), amount INT)'.format('book'))
+# base.commit()
 
+# r = cur.execute('SELECT*FROM book').fetchall()
+# print(r)
+# base.commit()
+
+#2
+# base.execute('INSERT INTO BOOK(book_id,title,author,price,amount) VALUES(17,"Молодая Гвардия",15,7,555)')
+# base.commit()
+#3
 r = cur.execute('SELECT*FROM book').fetchall()
 print(r)
+base.commit()
