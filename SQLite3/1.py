7c3477b52d6ee3fe41cf04d162ae97fc1fef0bf6
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-20 20:13:20 +0300

    Создал Таблицу с двумя текстовыми колонками, где id - это уникальный номер, в каждой строке их.

--- SQLite3/1.py
+++ SQLite3/1.py
@@ -4,3 +4,5 @@ import sqlite3
 conn = sqlite3.connect('name1.db')
 # Создаем объект cursor, который позволяет нам взаимодействовать с базой данных и добавлять записи
 cursor = conn.cursor()
+# Создадим таблицу с двумя текстовыми колонками
+cursor.execute('''CREATE TABLE IF NOT EXISTS tab_1(id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 TEXT,col_2 TEXT) ''')
