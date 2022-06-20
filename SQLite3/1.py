5eda05081fc1226e075766edb367d4b2d5d78f39
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-21 00:57:17 +0300

    -

--- SQLite3/1.py
+++ SQLite3/1.py
@@ -17,3 +17,8 @@ f = "black"
 cursor.execute('''CREATE TABLE IF NOT EXISTS tab_2(id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 TEXT,col_2 TEXT) ''')
 cursor.execute('''INSERT INTO tab_2(col_1,col_2) VALUES (?,?)''', (d, f))
 conn.commit()
+
+cursor.execute('''SELECT*FROM tab_2''')
+k = cursor.fetchall()
+print(k)
+
