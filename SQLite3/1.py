50a132572bf0060fc2b511ff696bd089c50cb2d7
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-22 23:43:01 +0300

    Удалил запись из таблицы по id, по значению

--- SQLite3/1.py
+++ SQLite3/1.py
@@ -12,13 +12,13 @@ cursor.execute('''INSERT INTO tab_1(col_1,col_2) VALUES ('hello','world')''')
 # Сохраняем изменения
 conn.commit()
 
-d = "red"
-f = "black"
-cursor.execute('''CREATE TABLE IF NOT EXISTS tab_2(id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 TEXT,col_2 TEXT) ''')
-cursor.execute('''INSERT INTO tab_2(col_1,col_2) VALUES (?,?)''', (d, f))
-conn.commit()
+# d = "red"
+# f = "black"
+# cursor.execute('''CREATE TABLE IF NOT EXISTS tab_2(id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 TEXT,col_2 TEXT) ''')
+# cursor.execute('''INSERT INTO tab_2(col_1,col_2) VALUES (?,?)''', (d, f))
+# conn.commit()
 
-cursor.execute('''SELECT*FROM tab_2''')
+cursor.execute('''SELECT*FROM tab_1''')
 k = cursor.fetchall()
 print(k)
 
