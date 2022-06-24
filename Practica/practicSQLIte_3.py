1ce1cfdb2eba24d459e583bdc30d3bfaaee6db87
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-24 18:19:19 +0300

    Приступил к выполнению пункта 4.

--- Practica/practicSQLIte_3.py
+++ Practica/practicSQLIte_3.py
@@ -14,6 +14,9 @@ import random
 conn = sqlite3.connect('z_3.db')
 cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS tab_1(id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 INTEGER) ''')
+r = random.randint(1,20)
+cursor.execute('''INSERT INTO tab_1 (col_1) VALUES (?)''',(r,))
+conn.commit()
 cursor.execute('''SELECT*FROM tab_1''')
 k = cursor.fetchall()
 print(k)
@@ -26,6 +29,10 @@ class A:
             if type(b) is int:
                 cursor.execute('''DELETE FROM tab_1 WHERE id = 1''')
                 conn.commit()
+        elif a is not None and b is not None and type(c) is int:
+            cursor.execute('''UPDATE tab_1 SET col_1 = 77 WHERE id = 3''')
+            conn.commit()
+
 example = A()
 example.baz(3,5)
 cursor.execute('''SELECT * FROM tab_1''')
