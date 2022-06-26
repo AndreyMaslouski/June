265547eb64ba1580e60d4171a1acd0603e31ddb6
Andrei Mas <masloyskiandrey@gmail.com>
2022-06-26 23:56:02 +0300

    Добавил в него ещё 3 кнопки. И кнопку "*" ещё.

--- PyQT5/1.py
+++ PyQT5/1.py
@@ -31,12 +31,55 @@ class Calculator (QWidget):
         self.num_3.resize(50, 50)
         self.num_3.move(115, 100)
 
-        self.div = QPushButton('/',self)
+        self.div = QPushButton('/', self)
         self.div.resize(50,50)
         self.div.move(170,100)
 
+        self.num_4 = QPushButton('4', self)
+        self.num_4.resize(50, 50)
+        self.num_4.move(5, 155)
+        # self.num_1.clicked.connect()
 
+        self.num_5 = QPushButton('5', self)
+        self.num_5.resize(50, 50)
+        self.num_5.move(60, 155)
 
+        self.num_6 = QPushButton('6', self)
+        self.num_6.resize(50, 50)
+        self.num_6.move(115, 155)
+
+        self.mul = QPushButton('*', self)
+        self.mul.resize(50, 50)
+        self.mul.move(170, 155)
+
+        # self.num_7 = QPushButton('7', self)
+        # self.num_7.resize(50, 50)
+        # self.num_7.move(5, 100)
+        # # self.num_1.clicked.connect()
+        #
+        # self.num_8 = QPushButton('8', self)
+        # self.num_8.resize(50, 50)
+        # self.num_8.move(60, 100)
+        #
+        # self.num_9 = QPushButton('9', self)
+        # self.num_9.resize(50, 50)
+        # self.num_9.move(115, 100)
+        #
+        # self.div = QPushButton('/',self)
+        # self.div.resize(50,50)
+        # self.div.move(170,100)
+        #
+        # self.div = QPushButton('*', self)
+        # self.div.resize(50, 50)
+        # self.div.move(170, 100)
+        #
+        # self.div = QPushButton('+', self)
+        # self.div.resize(50, 50)
+        # self.div.move(170, 100)
+        #
+        # self.div = QPushButton('-', self)
+        # self.div.resize(50, 50)
+        # self.div.move(170, 100)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
