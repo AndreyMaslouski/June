0af9c4ed20bffcf2417a339a8753b21dc5ac3048
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-03 19:17:18 +0300

    Создал с помощью цикла завершение игры. Если нажат "крестик", то Игра завершается.

--- D/PyGame/zmeika.py
+++ D/PyGame/zmeika.py
@@ -5,3 +5,17 @@ pygame.init()
 dis = pygame.display.set_mode((400,300))
 
 pygame.display.set_caption("Snake")
+
+blue = (0,0,255)
+red = (255,0,0)
+
+game_over = True
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+
+
+
