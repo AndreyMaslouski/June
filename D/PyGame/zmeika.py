87b2d486e4e17cba931eba4ac9ddcbbcf495667f
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-03 19:26:14 +0300

    Дополнил код, задав цветовой размер, и обновление дисплея. И команду выхода.

--- D/PyGame/zmeika.py
+++ D/PyGame/zmeika.py
@@ -15,6 +15,15 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        pygame.draw.rect(dis,blue,(200,150,10,10))
+        pygame.display.update()
+
+pygame = quit()
+quit()
+
+
+
+
 
 
 
