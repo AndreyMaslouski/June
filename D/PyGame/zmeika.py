4c68be0fad369f722a25e7403db01c95def790c8
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-03 20:10:33 +0300

    Задал ей координаты движений, чтобы двигалась в разные стороны (влево, вправо, вверх, вниз).

--- D/PyGame/zmeika.py
+++ D/PyGame/zmeika.py
@@ -2,11 +2,11 @@
 import pygame
 
 pygame.init()
-white = (255,255,255)
-blue = (0,0,255)
-red = (255,0,0)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+red = (255, 0, 0)
 
-dis = pygame.display.set_mode((400,300))
+dis = pygame.display.set_mode((400, 300))
 
 pygame.display.set_caption("Snake")
 
@@ -22,16 +22,23 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-        pygame.draw.rect(dis,blue,(200,150,10,10))
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.L_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -10
+            elif event.key == pygame.K_DOWN:
+                x1_change = 0
+                y1_change = 10
+
+        pygame.draw.rect(dis, blue, (200, 150, 10, 10))
         pygame.display.update()
 
 pygame = quit()
 quit()
-
-
-
-
-
-
-
-
