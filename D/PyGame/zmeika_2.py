b4219ab8f099c2b1364eb05a0a17a5562ed43d4d
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-04 21:32:46 +0300

    Поменял фон в игре, сделал его салатовым. Нарисовал белый квадратик, хочу ими сделать всё игровое поле.

---
+++ D/PyGame/zmeika_2.py
@@ -0,0 +1,78 @@
+# Написать игру "Змейка" в PyGame
+# Продолжить разработку игры. Доработать еду для змейки(змейка увеличивается при поедании чего-нибудь)
+#
+# По желанию добавить:
+#
+# -Фон
+#
+# -Счётчик очков
+
+
+
+import pygame
+
+
+pygame.init()
+white = (255, 255, 255)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
+Size_Block = 20
+Frame_Color = (0,255,204)
+
+dis = pygame.display.set_mode((800, 500))
+
+pygame.display.set_caption("Snake")
+
+game_over = False
+
+x1 = 300
+y1 = 400
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -10
+            elif event.key == pygame.K_DOWN:
+                x1_change = 0
+                y1_change = 10
+
+    if x1>=800 or x1<0 or y1>=600 or y1<0:
+        game_over = True
+
+    dis.fill(Frame_Color)
+    pygame.draw.rect(dis, white, [10, 20, Size_Block, Size_Block])
+
+    pygame.display.flip()
+
+
+    x1+=x1_change
+    y1+=y1_change
+
+    pygame.display.flip()
+
+    pygame.draw.rect(dis, blue, [x1, y1, 10, 10])
+    pygame.display.update()
+
+    clock.tick(30)
+
+pygame = quit()
+quit()
